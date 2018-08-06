7b3acd2b1f1476f0be764956a22fc4e2545927cc
Quang Luong <quangIO@outlook.com>
2018-08-06 20:39:52 +0700

    use env + built-in async

--- src/main/kotlin/edu/cutie/lightbackend/service/SearchService.kt
+++ src/main/kotlin/edu/cutie/lightbackend/service/SearchService.kt
@@ -2,13 +2,16 @@ package edu.cutie.lightbackend.service
 
 import edu.cutie.lightbackend.domain.ProductEntity
 import edu.cutie.lightbackend.helper.WithLogger
-import io.vertx.core.Vertx
-import io.vertx.kotlin.coroutines.dispatcher
-import kotlinx.coroutines.experimental.launch
+import io.vertx.core.json.Json
 import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.elasticsearch.action.ActionListener
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.common.xcontent.XContentType
 
 
 // TODO: Will change to codegen and proxy pattern later
@@ -18,18 +21,35 @@ interface SearchService {
 }
 
 val elasticsearchClient by lazy {
-  RestHighLevelClient(
-    RestClient.builder(
-      HttpHost("localhost", 9200, "http"),
-      HttpHost("localhost", 9201, "http")))
+  val host: String = System.getenv("ELASTICSEARCH_HOST") ?: "localhost"
+  val port: Int = System.getenv("ELASTICSEARCH_PORT")?.toInt() ?: 9200
+  val scheme: String = System.getenv("ELASTICSEARCH_SCHEME") ?: "http"
+  val username = System.getenv("ELASTICSEARCH_USER") ?: "elastic"
+  val password = System.getenv("ELASTICSEARCH_PASSWORD")
+
+  val credentialsProvider = BasicCredentialsProvider()
+  credentialsProvider.setCredentials(AuthScope.ANY,
+    UsernamePasswordCredentials(username, password))
+
+  val builder = RestClient.builder(HttpHost(host, port, scheme))
+    .setHttpClientConfigCallback { httpClientBuilder ->
+      httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+    }
+  RestHighLevelClient(builder)
 }
+
 class DefaultSearchService : SearchService, WithLogger {
   override suspend fun putIfAbsent(product: ProductEntity) {
-    launch(Vertx.currentContext().dispatcher()) {
-      val indexRequest = with(product) {
-        IndexRequest("product", "_doc", id.toString()).source("name", name)
-      }
-      elasticsearchClient.index(indexRequest)
+    val indexRequest = with(product) {
+      IndexRequest("product", "_doc", id.toString()).source(Json.encodePrettily(this), XContentType.JSON)
+      /*
+      .source("name", name,
+        "department", department,
+        "reviews", reviews
+      )
+      */
+
     }
+    elasticsearchClient.indexAsync(indexRequest, ActionListener.wrap {})
   }
 }
