7b28e7984a67cbd7a84196978974be77d43aa2e6
Quang Luong <quangIO@outlook.com>
2018-08-16 11:27:54 +0800

    update CI + review

--- src/main/kotlin/edu/cutie/lightbackend/domain/Review.kt
+++ src/main/kotlin/edu/cutie/lightbackend/domain/Review.kt
@@ -1,18 +1,20 @@
 package edu.cutie.lightbackend.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import edu.cutie.lightbackend.converter.StringListConverter
 import io.requery.*
 import java.sql.Timestamp
 
 
 @Entity
+@JsonIgnoreProperties("createdAt", allowGetters = true)
 interface Review: Persistable {
   @get:Key @get:ForeignKey(references = Person::class)
   var fromUser: Int
   @get:Key @get:ForeignKey(references = Product::class)
   var toProduct: Int
   var score: Int
-  var difficultyScore: Int
+  var difficulty: Int
   var comment: String
   var privateLevel: Int
   @get:Convert(StringListConverter::class)
