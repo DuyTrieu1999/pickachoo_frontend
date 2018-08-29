b12c16ae52d5b7d598a7d2eec091fee34d63681b
Quang Luong <quangIO@outlook.com>
2018-08-29 11:32:27 +0800

    optimize buffer allocation

--- src/main/kotlin/edu/cutie/lightbackend/helper/ResponseHelper.kt
+++ src/main/kotlin/edu/cutie/lightbackend/helper/ResponseHelper.kt
@@ -1,11 +1,12 @@
 package edu.cutie.lightbackend.helper
 
 import io.netty.handler.codec.http.HttpResponseStatus
+import io.vertx.core.http.HttpHeaders
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.json.Json
 
 fun HttpServerResponse.endWithJson(obj: Any? = null, code: HttpResponseStatus = HttpResponseStatus.OK) {
-  this.setStatusCode(code.code()).putHeader("Content-Type", "application/json; charset=utf-8").end(Json.encodePrettily(ResponseWithCode(obj, code.code())))
+  this.setStatusCode(code.code()).putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaders.createOptimized("application/json; charset=utf-8")).end(Json.encode(ResponseWithCode(obj, code.code())))
 }
 
 private data class ResponseWithCode(val content: Any? = null, val code: Int = HttpResponseStatus.OK.code())
