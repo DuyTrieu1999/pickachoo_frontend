3585fc36dc8f8f119c573d31010b337bdcec1ab5
Quang Luong <quangIO@outlook.com>
2018-09-19 18:26:44 +0800

    nani case sensitive?

--- src/main/kotlin/edu/cutie/lightbackend/MainVerticle.kt
+++ src/main/kotlin/edu/cutie/lightbackend/MainVerticle.kt
@@ -97,7 +97,7 @@ class MainVerticle : CoroutineVerticle() {
     route().handler(BodyHandler.create())
     route().handler(StaticHandler.create())
     route().handler(CorsHandler.create("http:\\/\\/localhost:\\[\\[0-9\\]+|https:\\/\\/qtmx\\.netlify\\.com|https:\\/\\/pickachoo\\.devel\\.faith|https:\\/\\/\\w+--qtmx\\.netlify\\.com")
-      .allowCredentials(true)
+      .allowCredentials(true).allowedHeader("Authorization")
     )
   }
 }
